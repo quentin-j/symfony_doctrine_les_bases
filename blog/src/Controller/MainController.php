f412ad0ab88cc012d9f572d778b47b8fa60646b3
quentin-j <quentin678jougla@gmail.com>
2021-06-28 12:47:33 +0200

    ajout des fonction update/delete + affichage de la BDD dans list.html.twig

---
+++ blog/src/Controller/MainController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/posts", name="list")
+     */
+    public function list(): Response
+    {
+        // récupérer la liste de tous les postes
+        $postRepository = $this->getDoctrine()->getRepository(Post::class);
+        $allPosts = $postRepository->findAll();
+
+        return $this->render('main/list.html.twig', [
+            'all_posts' => $allPosts
+        ]);
+    }
+
+    /**
+     * @Route("/post/create", name="create")
+     */
+    public function create(): Response
+    {
+        // créer un post 
+        $post = new Post();
+        $today = new DateTime();
+
+
+        $randomString = uniqid(); // <= permet d'avoir des chaine de caractère unique
+        $post->setTitle('Title ' . $randomString);
+        $post->setBody('body ' . $randomString);
+        $post->setNbLikes(rand(0, 500));
+
+        $post->setCreatedAt($today);
+        $post->setUpdatedAt($today);
+
+        // enregistre le post en BDD
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('list');
+    }
+    /**
+     * @Route("post/update/{id}", name="update")
+     */
+    public function update($id): Response
+    {
+        // récupèrer l'objet
+        $postRepository = $this->getDoctrine()->getRepository(Post::class);
+        $postToUpdate = $postRepository->find($id);
+
+        // mettre à jour 
+        $postToUpdate->setUpdatedAt(new DateTime());
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+
+        return $this->redirectToRoute('list');
+    }
+      /**
+     * @Route("post/delete/{id}", name="delete")
+     */
+    public function delete($id): Response
+    {
+        // récupèrer l'objet
+        $postRepository = $this->getDoctrine()->getRepository(Post::class);
+        $postToDelete = $postRepository->find($id);
+
+        // supprimer l'objet 
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($postToDelete);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('list');
+    }
+}
